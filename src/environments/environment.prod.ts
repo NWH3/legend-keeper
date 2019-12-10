86f7c0f18fc8d2c70996b0d211d8f7a8d82df1ea
Nathanial W. Heard <Nathanial.W.Heard@gmail.com>
2019-12-10 18:28:50 -0500

    Security update and environment properties correction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  worldBaseUrl: 'http://localhost:8800/world/',
-  gatewayBaseUrl: 'http://localhost:8800/'
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  worldBaseUrl: 'http://localhost:8800/world/',
-  gatewayBaseUrl: 'http://localhost:8800/'
+  production: false
 };
 
 /*
